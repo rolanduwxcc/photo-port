179b33e90c1f5e376c94c708c63bca6879168516
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-05-02 00:10:35 -0500

    nav updates

--- src/App.js
+++ src/App.js
@@ -9,31 +9,40 @@ import ContactForm from './components/Contact';
 import { capitalizeFirstLetter } from './utils/helpers';
 
 function App() {
-  const [categories] = useState([
-    { name: "commercial", description: "Photos of grocery stores, food trucks and other commercial projects"},
-    { name: "portraits", description: "Portraits of people in my life"},
-    { name: "food", description: "Delicious delicacies"},
-    { name: "landscape", description: "Fields, farmhouse, waterfalls, and the beauty of nature" },
-]);
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
-useEffect(() => {
-  document.title = capitalizeFirstLetter(currentCategory.name);
-}, [currentCategory]);
+  const [categories] = useState([
+    { name: "commercial", description: "Photos of grocery stores, food trucks and other commercial projects" },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    { name: "landscape", description: "Fields, farmhouse, waterfalls, and the beauty of nature" },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
 
   return (
     <div>
       <Nav
-        categories = {categories}
-        setCurrentCategory = {setCurrentCategory}
-        currentCategory = {currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       >
       </Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
